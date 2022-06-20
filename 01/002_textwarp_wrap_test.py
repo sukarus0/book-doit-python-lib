7fc7f4fdaa322a8372daee0288e413abbb34795b
sukarus0 <sukarus@gamil.com>
2022-06-20 07:59:27 +0000

    edit .gitignore | add test

--- 01/002_textwarp_wrap_test.py
+++ 01/002_textwarp_wrap_test.py
@@ -6,3 +6,12 @@ def test_textwrap_wrap():
     result = textwrap.wrap(long_text, width=70)
 
     assert len(result) == 6
+
+
+def test_textwrap_wrap_and_join():
+    long_text = "Life is too short, you need python."*10
+
+    result = textwrap.wrap(long_text, width=70)
+    result = ' '.join(result)
+
+    assert "Life is too short, you need python."*10 == result 
